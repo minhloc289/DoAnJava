b25d7b32834e1754f44699a824dccd59417fa767
Huyneef <22520571@gm.uit.edu.vn>
2024-05-22 16:22:06 +0700

    goi tap

---
+++ src/DAO/GoiTapDAO.java
@@ -0,0 +1,41 @@
+
+package DAO;
+
+import model.GOITAP;
+import database.JDBC;
+import java.sql.*;
+import java.util.ArrayList;
+import model.NhanVienAcc;
+
+public class GoiTapDAO implements DAOInterface<GOITAP> {
+     public static GoiTapDAO getInstance(){
+        return new GoiTapDAO();
+    }
+
+    @Override
+    public int insert(GOITAP t) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int update(GOITAP t) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int delete(GOITAP t) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ArrayList<GOITAP> selectAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public GOITAP selectById(String t) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+}
