0678875bd0496c707e9ad6798b7e1f3f2675d14b
Huyneef <22520571@gm.uit.edu.vn>
2024-05-28 00:59:40 +0700

    done GOitap

--- src/DAO/GoiTapDAO.java
+++ src/DAO/GoiTapDAO.java
@@ -16,28 +16,119 @@ public class GoiTapDAO implements DAOInterface<GOITAP> {
 
     @Override
     public int insert(GOITAP t) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-   
+        int ketQua = 0;
+        try {
+            Connection conn = JDBC.getConnection();
+            String sql = "INSERT INTO GOITAP (id_GT, tenGoi, moTa, giaTien) Values (?,?,?,?)";
+            PreparedStatement pst = conn.prepareStatement(sql);
+            pst.setString(1, t.getId_GT());
+            pst.setString(2, t.getTenGoi());
+            pst.setString(3, t.getMoTa());
+            pst.setDouble(4, t.getGiaTien());
+            
+            ketQua = pst.executeUpdate();
+
+        }
+        catch(SQLException e) {
+                e.printStackTrace();
+            
+        }   
+        return ketQua;
     }
 
     @Override
     public int update(GOITAP t) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+        int ketQua = 0;
+        try {
+            Connection conn = JDBC.getConnection();
+            String sql = "UPDATE GOITAP SET  tenGoi = ?, moTa = ?, giaTien = ? WHERE id_GT = ? AND isDeleted = 0";
+            PreparedStatement pst = conn.prepareStatement(sql);
+            pst.setString(4, t.getId_GT());
+            pst.setString(1, t.getTenGoi());
+            pst.setString(2, t.getMoTa());
+            pst.setDouble(3, t.getGiaTien());
+            
+            
+            ketQua = pst.executeUpdate();
+            
+        } catch (SQLException e) {
+            
+                e.printStackTrace();
+            
+        }
+        return ketQua;    }
 
     @Override
     public int delete(GOITAP t) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int ketQua = 0;
+        Connection conn = null;
+        CallableStatement stmt = null;
+        try {
+            conn = JDBC.getConnection();
+            String sqlCall = "{call XoaGoiTap(?)}";
+            stmt = conn.prepareCall(sqlCall);
+            stmt.setString(1, t.getId_GT());
+            stmt.execute();
+            
+        ketQua = 1;
+    } catch (SQLException e) {
+            e.printStackTrace();
+    }
+    finally {
+        try {
+            if (stmt != null) stmt.close();
+            if (conn != null) conn.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+    return ketQua;    
     }
 
     @Override
     public ArrayList<GOITAP> selectAll() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+        ArrayList<GOITAP> gtList = new ArrayList<>();
+        try {
+            Connection conn = JDBC.getConnection();
+            String sql = "SELECT * FROM GOITAP WHERE isDeleted = 0 ORDER BY Id_GT ASC";
+            PreparedStatement pst = conn.prepareStatement(sql);
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()) {
+                String Id_GT = rs.getString("Id_GT");
+                String MoTa = rs.getString("MoTa");
+                String TenGoi = rs.getString("TenGoi");
+                Double GiaTien = rs.getDouble("GiaTien");
+                
+                GOITAP gt = new GOITAP(Id_GT, TenGoi, MoTa, GiaTien);
+                gtList.add(gt);
+            }
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return gtList;       }
 
     @Override
     public GOITAP selectById(String t) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        GOITAP gt = null;
+        try {
+            Connection conn = JDBC.getConnection();
+            String sql = "SELECT * FROM GOITAP WHERE Id_GT = ? AND isDeleted = 0";
+            PreparedStatement pst = conn.prepareStatement(sql);
+            pst.setString(1, t);
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()) {
+                String Id_GT = rs.getString("Id_GT");
+                String MoTa = rs.getString("MoTa");
+                String TenGoi = rs.getString("TenGoi");
+                Double GiaTien = rs.getDouble("GiaTien");
+                gt = new GOITAP(Id_GT, MoTa, TenGoi, GiaTien);
+            }
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return gt;    
     }
     
     
